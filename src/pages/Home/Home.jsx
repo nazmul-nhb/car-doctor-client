4d767a2aaa5982aaab1d80ae0ab5ec3a203237db
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-05-03 22:21:43 +0600

    added services section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import { Helmet } from "react-helmet-async";
 import Banner from "./Banner/Banner";
 import About from "./About/About";
+import Services from "./Services/Services";
 
 const Home = () => {
     return (
@@ -10,6 +11,7 @@ const Home = () => {
             </Helmet>
             <Banner />
             <About />
+            <Services/>
         </main>
     );
 };
