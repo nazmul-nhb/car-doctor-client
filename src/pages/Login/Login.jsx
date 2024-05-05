1c3bee18c5c8eba57e89e4e24e17d12f6e03ba55
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-05-05 14:16:40 +0600

    set token in cookies

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,22 +1,35 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import img from '../../assets/images/login/login.svg'
 import { useContext } from 'react';
 import { AuthContext } from '../../providers/AuthProvider';
+import axios from 'axios';
 
 const Login = () => {
 
     const { signIn } = useContext(AuthContext);
+    const location = useLocation();
+    const navigate = useNavigate();
 
     const handleLogin = event => {
         event.preventDefault();
         const form = event.target;
         const email = form.email.value;
         const password = form.password.value;
-        console.log(name, email, password)
+
         signIn(email, password)
             .then(result => {
-                const user = result.user;
-                console.log(user);
+                const fireBaseUser = result.user;
+                console.log(fireBaseUser);
+                const user = { email }
+
+                // get access token
+                axios.post('http://localhost:5000/jwt', user, { withCredentials: true })
+                    .then(res => {
+                        console.log(res.data);
+                        if (res.data.success) {
+                            navigate(location?.state ? location?.state : '/');
+                        }
+                    })
             })
             .catch(error => console.log(error));
     }
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,11 +1,12 @@
 import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import PropTypes from 'prop-types';
 import loadingSpinner from '../assets/square-loading.svg'
 
 const PrivateRoute = ({ children }) => {
     const { user, loading } = useContext(AuthContext);
+    const location = useLocation();
 
     if (loading) {
         return (
@@ -18,7 +19,7 @@ const PrivateRoute = ({ children }) => {
     if (user?.email) {
         return children;
     }
-    return <Navigate to='/login' replace></Navigate>;
+    return <Navigate state={location.pathname} to='/login' replace></Navigate>;
 };
 
 PrivateRoute.propTypes = {
